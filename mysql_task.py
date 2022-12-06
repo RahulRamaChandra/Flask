7e053c07b8113cabd9159bf3fe645c787c9638cc
Rahul R. Chandran <T100262@techvantage.com>
2022-12-06 18:56:05 +0530

    Inserting data to the table from postman

--- mysql_task.py
+++ mysql_task.py
@@ -8,3 +8,17 @@ mydb = conn.connect(host = "localhost", user = "root", password = "Password#123"
 cursor = mydb.cursor()
 cursor.execute("create database if not exists tasksql")
 cursor.execute("create table if not exists tasksql.mysqltable(name varchar(30), number int)")
+
+@app.route('/insert', methods = ['POST'])
+
+def insert():
+    if request.method == 'POST':
+        name = request.json['name']
+        number = request.json['number']
+        cursor.execute("insert into tasksql.mysqltable values(%s , %s)" , (name,number))
+        mydb.commit()
+        return jsonify(str('succesfully inserted'))
+
+
+if __name__ == '__main__':
+    app.run()
